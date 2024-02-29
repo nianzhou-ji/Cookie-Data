4c79f140e13729007c5c936cd311f84fe9a12509
Your Name <you@example.com>
2024-02-29 19:36:11 +0800

    完成了临时资源收集的单网页应用

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import {IoMdAddCircle as AddIcon} from "react-icons/io";
 import {AiFillDelete as DeleteIcon} from "react-icons/ai";
 import {observer} from "mobx-react-lite";
 import MarkdownComp from "./components/MarkdownComp/MarkdownComp";
-import commonStore from "./stores/commonStore/commonStore";
 import {useStore} from "./stores";
 import indexedDBEngine from "./indexDBUtils/indexDBUtils";
 import ModalContainerComp from "./components/ModalComp/ModalComp";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import _ from "lodash";
 
 function App() {
     const {commonStore} = useStore()
@@ -20,8 +20,12 @@ function App() {
 
     const [addFileName, setAddFileName] = useState('default')
 
+    useEffect(() => {
+        commonStore.initDocumentsGroup()
+    }, []);
+
     return (
-        <div className="flex flex-col pl-1 pr-1 h-screen pt-2 pb-2">
+        <div className="flex flex-col pl-1 pr-1 h-screen pt-2 pb-2 w-screen">
             <div className="flex flee-none items-center justify-between mb-2">
                 <div className="flex items-center">
                     <TitleIcon size={'2rem'} className='ml-1'/>
@@ -29,7 +33,7 @@ function App() {
                 </div>
 
                 <div className='flex'>
-                    <h1 className={`font-bold mr-2 ${!commonStore.currentDocumentIsSaved ? 'text-red-500' : null}`}>
+                    <h1 className={`font-bold mr-2`}>
                         Document Title: {currentDocumentObj === undefined ? "" : currentDocumentObj.name}
                     </h1>
                     <div className="tooltip tooltip-left mr-2" data-tip="add document">
@@ -63,24 +67,77 @@ function App() {
                                  onClick={() => document.getElementById('addDocument_modal').showModal()}/>
                     </div>
                     <div className="tooltip tooltip-left mr-2" data-tip="delete document">
-                        <DeleteIcon size={'1.5rem'} className='cursor-pointer' onClick={() => {
+                        <DeleteIcon size={'1.5rem'} className='cursor-pointer' onClick={async () => {
                             commonStore.deleteDocumentsGroup(commonStore.currentDocumentID)
+
+                            try {
+                                await indexedDBEngine.open()
+                                await indexedDBEngine.delete(1)
+                                await indexedDBEngine.add({
+                                    id: 1,
+                                    documentsGroup: _.cloneDeep(commonStore.documentsGroup)
+                                })
+                                alert('delete success')
+                                if(commonStore.documentsGroup!==undefined && commonStore.documentsGroup.length>0){
+                                    commonStore.updateCurrentDocumentID(commonStore.documentsGroup[0].id)
+                                }
+                            } catch (e) {
+                                alert(`delete failed ${e}`)
+                            }
+
                         }}/>
                     </div>
                     <div className="tooltip tooltip-left mr-2" data-tip="view document list">
-                        <DocumentsIcon size={'1.5rem'} className='cursor-pointer'/>
+                        <DocumentsIcon size={'1.5rem'} className='cursor-pointer' htmlFor="my-drawer-4" onClick={() => {
+                            document.getElementById('drawerID').click()
+                        }}/>
+
+                        <ModalContainerComp>
+                            <div className="drawer">
+                                <input id="my-drawer-4" type="checkbox" className="drawer-toggle"/>
+                                <div className="drawer-content">
+                                    {/* Page content here */}
+                                    <label htmlFor="my-drawer-4" className="drawer-button btn btn-primary hidden"
+                                           id={'drawerID'}>Open drawer</label>
+                                </div>
+                                <div className="drawer-side">
+                                    <label htmlFor="my-drawer-4" aria-label="close sidebar"
+                                           className="drawer-overlay"></label>
+                                    <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+                                        {/* Sidebar content here */}
+                                        {commonStore.documentsGroup.map(item => <li key={item.id}
+                                                                                    className={`${item.id === commonStore.currentDocumentID ? 'bg-blue-100' : null}`}>
+                                            <a
+                                                onClick={() => commonStore.updateCurrentDocumentID(item.id)}>{item.name}</a>
+                                        </li>)}
+
+                                    </ul>
+                                </div>
+                            </div>
+                        </ModalContainerComp>
+
                     </div>
                     <div className="tooltip tooltip-left mr-2" data-tip='save document'>
                         <SaveIcon size={'1.5rem'} className='mr-1 cursor-pointer' onClick={async () => {
-                            commonStore.updateCurrentDocumentIsSaved(true)
-                            await indexedDBEngine.open()
-                            await indexedDBEngine.add({id: 1, name: 'Item 1'})
+                            try {
+                                await indexedDBEngine.open()
+                                await indexedDBEngine.delete(1)
+                                await indexedDBEngine.add({
+                                    id: 1,
+                                    documentsGroup: _.cloneDeep(commonStore.documentsGroup)
+                                })
+                                alert('save success')
+                            } catch (e) {
+                                alert(`save failed ${e}`)
+                            }
 
                         }}/>
                     </div>
 
                     <div className="tooltip tooltip-left" data-tip='export current document'>
-                        <ExportIcon size={'1.5rem'} className='cursor-pointer '/>
+                        <ExportIcon size={'1.5rem'} className='cursor-pointer' onClick={()=>{
+                            commonStore.downloadMarkdown()
+                        }}/>
                     </div>
 
                 </div>
--- src/stores/commonStore/commonStore.js
+++ src/stores/commonStore/commonStore.js
@@ -1,5 +1,6 @@
 import {makeAutoObservable} from "mobx";
 import _ from 'lodash'
+import indexedDBEngine from "../../indexDBUtils/indexDBUtils";
 
 class CommonStore {
 
@@ -11,19 +12,54 @@ class CommonStore {
         this.documentsGroup = tempObj
     }
 
+    updateDocumentsGroup(value){
+        this.documentsGroup =  value
+    }
+
+    downloadMarkdown() {
+        const obj  = this.getCurrentDocumentObj()
+
+        // 创建一个新的 Blob 对象，内容为 Markdown 文本
+        const blob = new Blob([obj.content], { type: 'text/markdown;charset=utf-8' });
+
+        // 创建一个指向 Blob 对象的 URL
+        const url = URL.createObjectURL(blob);
+
+        // 创建一个临时的 a 标签用于下载
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = obj.name+'.md';
+
+        // 触发下载
+        document.body.appendChild(a);
+        a.click();
+
+        // 清理
+        document.body.removeChild(a);
+        URL.revokeObjectURL(url);
+    }
+
+
+
+    async initDocumentsGroup(){
+        await indexedDBEngine.open()
+        const res = await indexedDBEngine.get(1)
+        this.updateDocumentsGroup(res.documentsGroup)
+
+        if(res.documentsGroup!==undefined && res.documentsGroup.length>0){
+            this.updateCurrentDocumentID(res.documentsGroup[0].id)
+        }
+
+
+
+    }
+
     deleteDocumentsGroup(id) {
         const tempObj = _.cloneDeep(this.documentsGroup)
         this.documentsGroup = tempObj.filter(item => item.id !== id)
     }
 
-
-    currentDocumentID = '456489'
-    currentDocumentIsSaved = true
-
-    updateCurrentDocumentIsSaved(value) {
-        this.currentDocumentIsSaved = value
-
-    }
+    currentDocumentID = ''
 
 
     patchDocumentsGroup(content) {
@@ -32,8 +68,6 @@ class CommonStore {
         if (res !== undefined) {
             res.content = content
             this.documentsGroup = tempObj
-            this.updateCurrentDocumentIsSaved(false)
-
         }
 
     }
